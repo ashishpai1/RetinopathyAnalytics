babd740b135277bace3819c0f4b29e6fbe5a629a
“ashishpai1” <ashish.pai.git@gmail.com>
2016-04-20 20:31:27 -0700

    added layout, controllers, partials

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 angular.module('myApp', ['ngRoute'])
 
 .config(function($routeProvider){
-  $routeProvider.when("/",
-    {
+  $routeProvider.when("/",{
       templateUrl: "partials/home.html",
       controller: "HomeCtrl"
-    }
-  );
+    }).when("/analytics",{
+	  templateUrl: "partials/dashboard.html",
+      controller: "AnalyticsCtrl"
+  }).when("/tryme",{
+	  templateUrl: "partials/tryMe.html",
+      controller: "TryMeCtrl"
+  })
 })
-
-
-
 .controller('HomeCtrl', ['$scope', '$http', function($scope, $http){
 	console.log("HomeCtrl");
+}])
+.controller('AnalyticsCtrl', ['$scope', '$http', function($scope, $http){
+	console.log("AnalyticsCtrl");
+}])
+.controller('TryMeCtrl', ['$scope', '$http', function($scope, $http){
+	console.log("TryMeCtrl");
 }]);
